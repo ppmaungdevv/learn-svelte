ef5db059498f00f23eda973bb06c47c34f86222f
ppmaungdev <ppmaungdev@gmail.com>
2022-08-21 17:55:31 +0630

    styling with tailwind

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -7,38 +7,41 @@
   const users = [
     {
       id: 1,
-      name: "U1",
-      mail: "u1@m.co",
+      name: "Manager_1",
+      mail: "manager_1@m.co",
       img: fml_user,
     },
     {
       id: 2,
-      name: "U2",
-      mail: "u2@m.co",
+      name: "Hr",
+      mail: "humanresource@m.co",
       img: shirt_user,
     },
     {
       id: 3,
-      name: "U3",
-      mail: "u3@m.co",
+      name: "Developer_1",
+      mail: "developer_1@m.co",
       img: fms_user,
     },
     {
       id: 4,
-      name: "U4",
-      mail: "u4@m.co",
+      name: "Developer_2",
+      mail: "developer_2@m.co",
       img: hood_user,
     },
   ];
 </script>
 
 <div>
-  <h1 class="text-3xl text-red-400">Users List</h1>
+  <h1 class="text-2xl text-center mt-10">Users List</h1>
   {#each users as user, index (user.id)}
-    <div>
-      <img src={user.img} alt={user.name} />
-      <p>{user.name}</p>
-      <p>{user.mail}</p>
+    <div
+      class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center"
+    >
+      <p class="w-10">{user.id}</p>
+      <img class="rounded-full w-14 h-14" src={user.img} alt={user.name} />
+      <p class="mx-4 w-20">{user.name}</p>
+      <p class="mx-4 w-20">{user.mail}</p>
     </div>
   {:else}
     <p>loading......</p>
