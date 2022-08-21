8cd8a517ed58b0bbcff51a19da2e7d02a7155e38
ppmaungdev <ppmaungdev@gmail.com>
2022-08-21 12:02:48 +0630

    fisrt component

---
+++ src/lib/Users.svelte
@@ -0,0 +1,19 @@
+<script>
+  const users = [
+    {
+      name: "U1",
+      mail: "u1@m.co",
+      img: "",
+    },
+  ];
+</script>
+
+<div>
+  <h1>Users List</h1>
+
+  <div>
+    <img src="" alt={users[0].name} />
+    <p>{users[0].name}</p>
+    <p>{users[0].mail}</p>
+  </div>
+</div>
