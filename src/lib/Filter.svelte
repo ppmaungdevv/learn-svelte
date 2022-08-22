195d4a3cc268f7af0dd54b12a20f49ac0604bf81
ppmaungdev <ppmaungdev@gmail.com>
2022-08-22 20:37:38 +0630

    event dispatcher same as emit event in vue

---
+++ src/lib/Filter.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  const filter = (e) => {
+    dispatch("filter", e.target.value);
+  };
+</script>
+
+<select
+  on:change={filter}
+  name="user-filter"
+  class="py-2 px-4 bg-white border rounded ml-4"
+>
+  <option value={null}>All</option>
+  <option value={true}>Active</option>
+  <option value={false}>Inactive</option>
+</select>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -4,6 +4,7 @@
   import shirt_user from "../assets/img/shirt_user.png";
   import hood_user from "../assets/img/hood_user.png";
   import User from "./User.svelte";
+  import Filter from "./Filter.svelte";
 
   const users = [
     {
@@ -36,28 +37,31 @@
     },
   ];
   let filtered_users = users;
+  // ------ normal event
+  // const filter = (e) => {
+  //   if (e.target.value === "null") {
+  //     filtered_users = users;
+  //     return;
+  //   }
+  //   const status = e.target.value === "true";
+  //   console.log(status);
+  //   filtered_users = users.filter((user) => user.active == status);
+  // };
+  // -------- dispatch event
   const filter = (e) => {
-    if (e.target.value === "null") {
-      filtered_users = users;
+    const { detail } = e;
+    console.log(typeof detail);
+    if (detail === "null") {
       return;
     }
-    const status = e.target.value === "true";
-    console.log(status);
+    const status = detail === "true";
     filtered_users = users.filter((user) => user.active == status);
   };
 </script>
 
 <div>
   <h1 class="text-2xl text-center mt-10">Users List</h1>
-  <select
-    on:change={filter}
-    name="user-filter"
-    class="py-2 px-4 bg-white border rounded ml-4"
-  >
-    <option value={null}>All</option>
-    <option value={true}>Active</option>
-    <option value={false}>Inactive</option>
-  </select>
+  <Filter on:filter={filter} />
   <!-- {#each users as { id, name, mail, img }, index (id)} -->
   {#each filtered_users as user, index (user.id)}
     <!-- <User {id} {name} {mail} {img} /> -->
