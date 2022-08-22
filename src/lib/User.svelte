6073655ddba53d278e70864039e7fc8a6734e14d
ppmaungdev <ppmaungdev@gmail.com>
2022-08-22 19:26:17 +0630

    props

---
+++ src/lib/User.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { identity } from "svelte/internal";
+
+  export let user;
+  //   export let id, img, name, mail;
+</script>
+
+<div
+  class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center"
+>
+  <p class="w-10">{user.id}</p>
+  <img class="rounded-full w-14 h-14" src={user.img} alt={user.name} />
+  <p class="mx-4 w-20">{user.name}</p>
+  <p class="mx-4 w-20">{user.mail}</p>
+</div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -3,6 +3,7 @@
   import fms_user from "../assets/img/fms_user.png";
   import shirt_user from "../assets/img/shirt_user.png";
   import hood_user from "../assets/img/hood_user.png";
+  import User from "./User.svelte";
 
   const users = [
     {
@@ -10,39 +11,38 @@
       name: "Manager_1",
       mail: "manager_1@m.co",
       img: fml_user,
+      active: true,
     },
     {
       id: 2,
       name: "Hr",
       mail: "humanresource@m.co",
       img: shirt_user,
+      active: true,
     },
     {
       id: 3,
       name: "Developer_1",
       mail: "developer_1@m.co",
       img: fms_user,
+      active: true,
     },
     {
       id: 4,
       name: "Developer_2",
       mail: "developer_2@m.co",
       img: hood_user,
+      active: false,
     },
   ];
 </script>
 
 <div>
   <h1 class="text-2xl text-center mt-10">Users List</h1>
+  <!-- {#each users as { id, name, mail, img }, index (id)} -->
   {#each users as user, index (user.id)}
-    <div
-      class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center"
-    >
-      <p class="w-10">{user.id}</p>
-      <img class="rounded-full w-14 h-14" src={user.img} alt={user.name} />
-      <p class="mx-4 w-20">{user.name}</p>
-      <p class="mx-4 w-20">{user.mail}</p>
-    </div>
+    <!-- <User {id} {name} {mail} {img} /> -->
+    <User {user} />
   {:else}
     <p>loading......</p>
   {/each}
