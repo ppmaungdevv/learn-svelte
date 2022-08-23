920040916de77995b7f70c4b4ee7640e3868760e
ppmaungdev <ppmaungdev@gmail.com>
2022-08-23 20:48:18 +0630

    slot part-1

--- src/lib/AddModal.svelte
+++ src/lib/AddModal.svelte
@@ -23,10 +23,15 @@
         class="relative bg-white rounded-lg px-4 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6"
       >
         <div class="mt-5 sm:mt-6">
-          <button
-            class="px-2 py-1 bg-white border rounded hover:bg-gray-200"
-            on:click={close}>Close</button
-          >
+          <slot />
+          <div class="flex justify-between">
+            <button
+              class="px-2 py-1 bg-white border rounded hover:bg-gray-200"
+              on:click={close}>Close</button
+            >
+            <slot name="submit-btn" />
+            <!-- Element with a slot='...' (a.k.a name_slot) attribute must be a child of a component or a descendant of a custom element -->
+          </div>
         </div>
       </div>
     </div>
--- src/lib/AddUser.svelte
+++ src/lib/AddUser.svelte
@@ -1,6 +1,6 @@
 <script>
   import AddModal from "./AddModal.svelte";
-  let show_modal = false;
+  let show_modal = true;
 </script>
 
 <button
@@ -8,5 +8,21 @@
   class="px-4 py-1 rounded border text-sky-700 border-sky-500">Add User</button
 >
 {#if show_modal}
-  <AddModal on:close={() => (show_modal = false)} />
+  <AddModal on:close={() => (show_modal = false)}>
+    <h3 class="text-2xl text-center">Add New</h3>
+    <form action="">
+      <div>
+        <label for="name">Name</label>
+        <input
+          class="border border-sky-500 rounded p-2"
+          type="text"
+          name=""
+          id="name"
+        />
+      </div>
+    </form>
+    <button slot="submit-btn" class="border border-blue-500 px-2 py-1 rounded"
+      >Add User</button
+    >
+  </AddModal>
 {/if}
